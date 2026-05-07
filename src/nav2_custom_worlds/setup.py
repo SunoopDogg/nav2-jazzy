d117c21b2e9f2337ea8287bb0f6f7602b7cd4903
SunoopDogg <aswoo55555@gmail.com>
2026-05-08 02:56:28 +0900

    feat: scaffold nav2_custom_worlds ament_python package

---
+++ src/nav2_custom_worlds/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'nav2_custom_worlds'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='aswoo55555@gmail.com',
+    description='Nav2 simulation launch files for AWS RoboMaker worlds',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [],
+    },
+)
